82d82fabb7906183dbe9d386fed75ca3e4df8baf
bismarckx <klimentmaslennikov@gmail.com>
2023-11-06 19:17:04 +0300

    feat: add base features

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @ApiProperty({
+    example: 'user@mail.ru',
+    description: 'Почтовый адрес',
+  })
+  readonly email: string;
+
+  @ApiProperty({
+    example: 'AsafSasAd12d!@#',
+    description: 'Пароль',
+  })
+  readonly password: string;
+}
